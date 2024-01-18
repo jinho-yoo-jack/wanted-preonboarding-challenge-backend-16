8812744fc40b01ed74ab71a4c9ff95762fa28880
SeongHo5 <newfuring1123@gmail.com>
2024-01-18 12:03:02 +0900

    cleanup: Remove previos codes
    - 직접 해보기 도전..!

--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/Performance.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/Performance.java
@@ -1,38 +1,57 @@
 package com.wanted.preonboarding.ticket.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.id.UUIDGenerator;
+import org.hibernate.id.uuid.UuidGenerator;
 
-import java.sql.Date;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
-@Table
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-public class Performance {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "performance", uniqueConstraints = {
+        @UniqueConstraint(name = "performance_id_uk", columnNames = "id"),
+        @UniqueConstraint(name = "performance_round_uk", columnNames = "round")
+})
+public class Performance extends BaseEntity {
+
     @Id
+    @Comment("공연 정보 ID")
     @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)")
+    @GenericGenerator(name="uuid2", type = UuidGenerator.class)
+    @Column(name = "id", columnDefinition = "BINARY(16)")
     private UUID id;
-    @Column(nullable = false)
+
+    @Comment("공연명")
+    @Column(name = "name", nullable = false)
     private String name;
-    @Column(nullable = false)
-    private int price;
-    @Column(nullable = false)
-    private int round;
-    @Column(nullable = false)
-    private int type;
-    @Column(nullable = false)
-    private Date start_date;
-    @Column(nullable = false, name = "is_reserve", columnDefinition = "varchar default 'disable'")
+
+    @Comment("가격")
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Comment("회차 정보")
+    @Column(name = "round", nullable = false)
+    private Integer round;
+
+    @Comment("공연 구분 - 콘서트, 전시회, 기타")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private PerformanceType type;
+
+    @Comment("공연 시작일")
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Comment("예약 가능 여부")
+    @Column(name = "is_reserve", nullable = false, columnDefinition = "varchar(255) default 'disable'")
     private String isReserve;
 
 }
