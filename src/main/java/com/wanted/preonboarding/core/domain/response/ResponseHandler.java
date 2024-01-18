cf504d1cfa6b8d4124c5115e45fee1a0e0278f58
SeongHo5 <newfuring1123@gmail.com>
2024-01-18 20:24:45 +0900

    feat: Add response create methods
    - 응답 생성 메서드와, 메세지 상수 추가

--- src/main/java/com/wanted/preonboarding/core/domain/response/ResponseHandler.java
+++ src/main/java/com/wanted/preonboarding/core/domain/response/ResponseHandler.java
@@ -1,9 +1,23 @@
 package com.wanted.preonboarding.core.domain.response;
 
 import lombok.Builder;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Builder
 public record ResponseHandler<T>(HttpStatus statusCode, String message, T data) {
+    public static final String MESSAGE_SUCCESS = "success";
+    public static final String MESSAGE_FAIL = "fail";
+
+    public static <T> ResponseEntity<ResponseHandler<T>> createResponse(HttpStatus status, String message, T data) {
+        return ResponseEntity.status(status)
+                .body(
+                        ResponseHandler.<T>builder()
+                                .statusCode(status)
+                                .message(message)
+                                .data(data)
+                                .build()
+                );
+    }
+
 }
