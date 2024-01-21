b7da7691dee359dd94b91eef39bb15154a9b168f
SeongHo5 <newfuring1123@gmail.com>
2024-01-21 15:17:08 +0900

    refactor: Refactor early bird strategy to use Clock
    - 얼리버드 할인 전략에서 날짜 비교 시, Clock 객체를 통해 현재 날짜를 가져오도록 수정

---
+++ src/main/java/com/wanted/preonboarding/ticket/application/config/TimeConfig.java
@@ -0,0 +1,16 @@
+package com.wanted.preonboarding.ticket.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class TimeConfig {
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+
+}
--- src/main/java/com/wanted/preonboarding/ticket/application/service/strategy/EarlyBirdDiscountStrategy.java
+++ src/main/java/com/wanted/preonboarding/ticket/application/service/strategy/EarlyBirdDiscountStrategy.java
@@ -4,23 +4,26 @@ import com.wanted.preonboarding.ticket.domain.entity.Reservation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
 public class EarlyBirdDiscountStrategy implements DiscountStrategy {
 
-    // Clock 적용 해보기
     public static final String NAME = "얼리버드 할인";
-    public static final int EARLY_BIRD_MONTH = 3;
-    public static final int EARLY_BIRD_DISCOUNT = 15;
+    public static final int MIN_MONTHS_BEFORE_PERFORMANCE_FOR_EARLY_BIRD = 3;
+    public static final int EARLY_BIRD_DISCOUNT_RATE = 15;
+
+    private final Clock clock;
+
     @Override
     public int caculateDiscount(Reservation reservation) {
-        int price = reservation.getPerformanceSeatInfo().getPerformance().getPrice();
-        if (isEarlyBird(reservation)) {
-            price = (int) (price * (1 - (EARLY_BIRD_DISCOUNT / 100.0)));
+        int originalPrice = reservation.getPerformanceSeatInfo().getPerformance().getPrice();
+        if (isEligibleForEarlyBirdDiscount(reservation)) {
+            return applyDiscount(originalPrice);
         }
-        return price;
+        return originalPrice;
     }
 
     @Override
@@ -28,9 +31,15 @@ public class EarlyBirdDiscountStrategy implements DiscountStrategy {
         return NAME;
     }
 
-    private boolean isEarlyBird(Reservation reservation) {
-        LocalDateTime performanceDate = reservation.getPerformanceSeatInfo().getPerformance().getStartDate();
-        LocalDateTime reservationDate = LocalDateTime.now();
-        return performanceDate.minusMonths(EARLY_BIRD_MONTH).isBefore(reservationDate);
+    private boolean isEligibleForEarlyBirdDiscount(Reservation reservation) {
+        LocalDateTime performanceStartDate = reservation.getPerformanceSeatInfo().getPerformance().getStartDate();
+        LocalDateTime currentDateTime = LocalDateTime.now(clock);
+
+        return performanceStartDate.minusMonths(MIN_MONTHS_BEFORE_PERFORMANCE_FOR_EARLY_BIRD).isBefore(currentDateTime);
     }
+
+    private int applyDiscount(int price) {
+        return (int) (price * (1 - (EARLY_BIRD_DISCOUNT_RATE / 100.0)));
+    }
+
 }
