455ab808ae741e656adf4f51e593d691d1159bc9
cheorhyeon <r4560798@naver.com>
2024-01-22 20:59:03 +0900

    feat : PerformanceSeatInfo Entity 정의

--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/PerformanceSeatInfo.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/PerformanceSeatInfo.java
@@ -1,4 +1,66 @@
 package com.wanted.preonboarding.ticket.domain.entity;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+	uniqueConstraints =
+	@UniqueConstraint(
+		name = "performance_seat_info_unique",
+		columnNames = {"performance_id", "round", "line", "seat"}
+	)
+)
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class PerformanceSeatInfo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(columnDefinition = "BINARY(16)", nullable = false, name = "performance_id")
+	private UUID performanceId;
+
+	@Column(nullable = false)
+	private int round;
+
+	@Column(nullable = false)
+	private int gate;
+
+	@Column(nullable = false)
+	private String line;
+
+	@Column(nullable = false)
+	private int seat;
+
+	@Column(nullable = false, name = "is_reserve", columnDefinition = "varchar(255) default 'enable'")
+	private String isReserve;
+
+	@CreatedDate
+	@NotNull
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@NotNull
+	private LocalDateTime updatedAt;
 }
