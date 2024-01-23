d3f9259bb26fe903a591a06ea1012f18dc9a52fd
cheorhyeon <r4560798@naver.com>
2024-01-23 17:14:15 +0900

    fix : 절대 경로 수정

--- src/main/java/com/wanted/preonboarding/ticket/presentation/QueryController.java
+++ src/main/java/com/wanted/preonboarding/ticket/presentation/QueryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("query")
+@RequestMapping("/query")
 @RequiredArgsConstructor
 public class QueryController {
     private final TicketSeller ticketSeller;
