9f819dc1240babe8df34eceafbaf4fc3b36d6331
bbbbooo <qlqlzbxkzl@naver.com>
2024-01-24 00:49:32 +0900

    feat : 좌석 정보에 좌석 구역 추가함

--- src/main/java/com/wanted/preonboarding/ticketing/domain/vo/SeatInfo.java
+++ src/main/java/com/wanted/preonboarding/ticketing/domain/vo/SeatInfo.java
@@ -6,7 +6,10 @@ import org.hibernate.annotations.Comment;
 @Embeddable
 public class SeatInfo {
     @Comment("좌석 번호")
-    private int no;
+    private int number;
+
+    @Comment("좌석 구역")
+    private char section;
 
     @Comment("좌석 예약 가능 여부")
     private Boolean IsReserved;
