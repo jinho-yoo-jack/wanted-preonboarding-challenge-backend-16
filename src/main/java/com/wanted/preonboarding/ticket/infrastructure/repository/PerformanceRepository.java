604acd81b59615f05eb80818a4c03fb0b3c36c89
cheorhyeon <r4560798@naver.com>
2024-01-23 21:52:36 +0900

    feat : 공연 및 전시 정보 조회 기능 추가

--- src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/PerformanceRepository.java
+++ src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/PerformanceRepository.java
@@ -1,6 +1,9 @@
 package com.wanted.preonboarding.ticket.infrastructure.repository;
 
 import com.wanted.preonboarding.ticket.domain.entity.Performance;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +13,6 @@ import java.util.UUID;
 public interface PerformanceRepository extends JpaRepository<Performance, UUID> {
     Optional<Performance> findById(UUID id);
     List<Performance> findByIsReserve(String isReserve);
+    Page<Performance> findByIsReserve(String isReserve, PageRequest pageRequest);
     Performance findByName(String name);
 }
