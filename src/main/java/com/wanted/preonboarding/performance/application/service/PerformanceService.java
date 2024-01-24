7e85bfa0eb3b2c55ce0c5ed253bb75d1fd37cce0
JangAJang <janghee5395@gmail.com>
2024-01-24 20:29:21 +0900

    feat: 좌석이 예약될 때, 예약 도메인에서 발생시킬 이벤트 구현

---
+++ src/main/java/com/wanted/preonboarding/performance/application/service/PerformanceService.java
@@ -0,0 +1,13 @@
+package com.wanted.preonboarding.performance.application.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PerformanceService {
+
+
+}
