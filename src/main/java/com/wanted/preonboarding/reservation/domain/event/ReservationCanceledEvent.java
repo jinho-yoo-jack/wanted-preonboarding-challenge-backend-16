42f8b39e9fe56e64b23e89b10d64853d8ac42209
JangAJang <janghee5395@gmail.com>
2024-01-24 20:40:16 +0900

    feat: 이벤트의 정적 팩토리 메서드 구현

--- src/main/java/com/wanted/preonboarding/reservation/domain/event/ReservationCanceledEvent.java
+++ src/main/java/com/wanted/preonboarding/reservation/domain/event/ReservationCanceledEvent.java
@@ -4,9 +4,16 @@ import com.wanted.preonboarding.common.model.SeatInfo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @AllArgsConstructor
 @Getter
 public class ReservationCanceledEvent {
 
     private final SeatInfo seatInfo;
+    private final UUID performanceId;
+
+    public static ReservationCanceledEvent of(final SeatInfo seatInfo,final UUID performanceId) {
+        return new ReservationCanceledEvent(seatInfo, performanceId);
+    }
 }
