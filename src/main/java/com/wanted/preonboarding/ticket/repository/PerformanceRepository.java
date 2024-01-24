12d68934f27107013b727a8e0a9ce32583d25811
leeedohyun <ldh19738@gmail.com>
2024-01-24 22:07:22 +0900

    test: 아이디로 공연 조회 메서드 테스트

--- src/main/java/com/wanted/preonboarding/ticket/repository/PerformanceRepository.java
+++ src/main/java/com/wanted/preonboarding/ticket/repository/PerformanceRepository.java
@@ -1,5 +1,6 @@
 package com.wanted.preonboarding.ticket.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.wanted.preonboarding.ticket.domain.entity.Performance;
 
 public interface PerformanceRepository extends JpaRepository<Performance, UUID> {
-//    List<Performance> findByIsReserve(String isReserve);
+
+    Optional<Performance> findById(UUID id);
+
     Performance findByName(String name);
 }
---
+++ src/test/java/com/wanted/preonboarding/ticket/repository/PerformanceRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.wanted.preonboarding.ticket.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.wanted.preonboarding.ticket.domain.entity.Performance;
+import com.wanted.preonboarding.ticket.domain.entity.PerformanceType;
+import com.wanted.preonboarding.ticket.domain.entity.ReservationStatus;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PerformanceRepositoryTest {
+
+    @Autowired
+    private PerformanceRepository performanceRepository;
+
+    @Test
+    void 아이디로_공연_조회() {
+        // given
+        final Performance performance = Performance.builder()
+                .id(UUID.fromString("4438a3e6-b01c-11ee-9426-0242ac180002"))
+                .name("공연 이름")
+                .price(10000)
+                .type(PerformanceType.EXHIBITION)
+                .status(ReservationStatus.AVAILABLE)
+                .start_date(LocalDateTime.of(2024, 2, 1, 10, 0))
+                .build();
+        performanceRepository.save(performance);
+
+        // when
+        final Performance findPerformance = performanceRepository.findById(performance.getId()).get();
+
+        // then
+        assertThat(findPerformance).usingRecursiveComparison()
+                .isEqualTo(performance);
+    }
+}
