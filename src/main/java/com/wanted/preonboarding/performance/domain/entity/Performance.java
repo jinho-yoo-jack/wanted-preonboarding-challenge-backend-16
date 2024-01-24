6ac08c41da5f7a17172d274b57484c4c9da1a6b3
JangAJang <janghee5395@gmail.com>
2024-01-24 19:39:06 +0900

    feat: Performance 객체 구현

---
+++ src/main/java/com/wanted/preonboarding/performance/domain/entity/Performance.java
@@ -0,0 +1,46 @@
+package com.wanted.preonboarding.performance.domain.entity;
+
+import com.wanted.preonboarding.common.model.DefaultEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Performance extends DefaultEntity {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int round;
+
+    @Column(nullable = false)
+    private int type;
+
+    @Column(nullable = false, name = "start_date")
+    private Date startDate;
+
+    @Column(nullable = false, name = "is_reserve", columnDefinition = "varchar default 'disable'")
+    private String isReserve;
+
+    public boolean canReserve() {
+        return this.isReserve.equals("enable");
+    }
+}
