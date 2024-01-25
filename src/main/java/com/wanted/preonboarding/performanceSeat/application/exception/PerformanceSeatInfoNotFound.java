df6667a6380b91f25fc579c45e082382e10c9325
JangAJang <janghee5395@gmail.com>
2024-01-25 12:42:40 +0900

    feat: PerformanceSeatInfo의 예외 핸들러 구현

--- src/main/java/com/wanted/preonboarding/performanceSeat/application/exception/PerformanceSeatInfoNotFound.java
+++ src/main/java/com/wanted/preonboarding/performanceSeat/application/exception/PerformanceSeatInfoNotFound.java
@@ -2,7 +2,13 @@ package com.wanted.preonboarding.performanceSeat.application.exception;
 
 public class PerformanceSeatInfoNotFound extends RuntimeException{
 
+    private static final String MESSAGE = "공연 좌석을 찾을 수 없습니다.";
+
     public PerformanceSeatInfoNotFound() {
-        super("공연 좌석을 찾을 수 없습니다.");
+        super(MESSAGE);
+    }
+
+    public static String getExceptionMessage() {
+        return MESSAGE;
     }
 }
