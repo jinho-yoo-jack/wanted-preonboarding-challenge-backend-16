799d169837a540ef85f49f99964379889c97b4ac
JangAJang <janghee5395@gmail.com>
2024-01-25 13:51:05 +0900

    feat: 이벤트 위임으로 공연좌석 예약 취소시 공연이 예약 가능하게 수정되는 기능 구현

--- src/main/java/com/wanted/preonboarding/performance/application/service/PerformanceService.java
+++ src/main/java/com/wanted/preonboarding/performance/application/service/PerformanceService.java
@@ -3,6 +3,7 @@ package com.wanted.preonboarding.performance.application.service;
 import com.wanted.preonboarding.performance.application.exception.PerformanceNotFoundException;
 import com.wanted.preonboarding.performance.domain.entity.Performance;
 import com.wanted.preonboarding.performance.infrasturcture.repository.PerformanceRepository;
+import com.wanted.preonboarding.performanceSeat.domain.event.EnablePerformanceReservationEvent;
 import com.wanted.preonboarding.performanceSeat.domain.event.SeatSoldOutEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,5 +26,12 @@ public class PerformanceService {
                 .orElseThrow(PerformanceNotFoundException::new);
 
         performance.disableReservation();
+    }
+
+    @EventListener(EnablePerformanceReservationEvent.class)
+    @Transactional
+    public void updatePerformanceEnabled(final EnablePerformanceReservationEvent enablePerformanceReservationEvent) {
+        Performance performance = enablePerformanceReservationEvent.getPerformance();
+        performance.enableReservation();
     }
 }
