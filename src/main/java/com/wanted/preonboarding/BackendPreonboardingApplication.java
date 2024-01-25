df561ba438e9e61221da23781c99debc2d0a3d1a
bbbbooo <qlqlzbxkzl@naver.com>
2024-01-26 01:39:54 +0900

    style : 누락 어노테이션 추가함

--- src/main/java/com/wanted/preonboarding/BackendPreonboardingApplication.java
+++ src/main/java/com/wanted/preonboarding/BackendPreonboardingApplication.java
@@ -2,8 +2,12 @@ package com.wanted.preonboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableJpaAuditing
 public class BackendPreonboardingApplication {
 
     public static void main(String[] args) {
