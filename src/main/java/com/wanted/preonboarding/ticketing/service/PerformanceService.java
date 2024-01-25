4a856b6cc3e759f79238475bb5975eae08d5e4bb
bbbbooo <qlqlzbxkzl@naver.com>
2024-01-26 03:14:43 +0900

    refactor : isReserve가 enable이 아닐 경우 예외처리 되도록 구현함

--- src/main/java/com/wanted/preonboarding/ticketing/service/PerformanceService.java
+++ src/main/java/com/wanted/preonboarding/ticketing/service/PerformanceService.java
@@ -1,5 +1,7 @@
 package com.wanted.preonboarding.ticketing.service;
 
+import com.wanted.preonboarding.ticketing.aop.advice.exception.DefaultException;
+import com.wanted.preonboarding.ticketing.aop.advice.payload.ErrorCode;
 import com.wanted.preonboarding.ticketing.domain.dto.response.ReadPerformanceResponse;
 import com.wanted.preonboarding.ticketing.domain.entity.Performance;
 import com.wanted.preonboarding.ticketing.repository.PerformanceRepository;
@@ -11,9 +13,15 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PerformanceService {
+    private static final String POSSIBLE_RESERVATION = "enable";
+
     private final PerformanceRepository performanceRepository;
 
     public Page<ReadPerformanceResponse> read(String isReserve, Pageable pageable) {
+        if (!isReserve.equals(POSSIBLE_RESERVATION)) {
+            throw new DefaultException(ErrorCode.NOT_VALIDATED_PARAM);
+        }
+
         Page<Performance> performances = performanceRepository.findByIsReserve(isReserve, pageable);
 
         return performances.map(Performance::toReadPerformanceResponse);
