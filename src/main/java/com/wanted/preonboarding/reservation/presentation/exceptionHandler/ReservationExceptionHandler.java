ec00f17b0efbd9312697a09abadc192eba6f5fdc
JangAJang <janghee5395@gmail.com>
2024-01-25 13:37:28 +0900

    feat: 예약 삭제시도시 예약이 존재하지 않으면 발생시키는 예외의 핸들러 구현

--- src/main/java/com/wanted/preonboarding/reservation/presentation/exceptionHandler/ReservationExceptionHandler.java
+++ src/main/java/com/wanted/preonboarding/reservation/presentation/exceptionHandler/ReservationExceptionHandler.java
@@ -3,6 +3,7 @@ package com.wanted.preonboarding.reservation.presentation.exceptionHandler;
 import com.wanted.preonboarding.core.domain.response.ResponseHandler;
 import com.wanted.preonboarding.reservation.application.exception.NotReservedYet;
 import com.wanted.preonboarding.reservation.application.exception.ReservationAlreadyExists;
+import com.wanted.preonboarding.reservation.application.exception.ReservationNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +24,14 @@ public class ReservationExceptionHandler {
         return ResponseHandler.builder()
                 .statusCode(HttpStatus.NOT_FOUND)
                 .message(NotReservedYet.getExceptionMessage())
+                .build();
+    }
+
+    @ExceptionHandler(ReservationNotFound.class)
+    public ResponseHandler<Object> reservationNotFound() {
+        return ResponseHandler.builder()
+                .statusCode(HttpStatus.NOT_FOUND)
+                .message(ReservationNotFound.getExceptionMessage())
                 .build();
     }
 }
