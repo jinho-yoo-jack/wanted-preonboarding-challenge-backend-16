7d11dbcb5e98594f116074cb50f956c04f7cc97a
bbbbooo <qlqlzbxkzl@naver.com>
2024-01-26 14:02:59 +0900

    refactor : delete 연산을 in절로 실행하기 위하여 jpql로 쿼리문을 작성함
    - 기존의 deleteAllInBatch는 or 연산을 사용하기 때문에 성능상 이점을 가지지 못한다고 판단하여 수정함

--- src/main/java/com/wanted/preonboarding/ticketing/repository/AlarmRepository.java
+++ src/main/java/com/wanted/preonboarding/ticketing/repository/AlarmRepository.java
@@ -3,6 +3,8 @@ package com.wanted.preonboarding.ticketing.repository;
 import com.wanted.preonboarding.ticketing.domain.entity.Alarm;
 import com.wanted.preonboarding.ticketing.domain.entity.Performance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,8 @@ import java.util.List;
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     List<Alarm> findAllByPerformance(Performance performance);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Alarm a where a in :alarms")
+    void deleteAllInBatch(List<Alarm> alarms);
 }
