35060dd2d2eb757989c2ffd7048cd8ce6f28a55d
JangAJang <janghee5395@gmail.com>
2024-01-26 14:56:08 +0900

    feat: 예약가능한 공연의 id를 통해 좌석들을 조회하는 기능 구현

---
+++ src/main/java/com/wanted/preonboarding/performanceSeat/presentation/controller/PerformanceSeatController.java
@@ -0,0 +1,28 @@
+package com.wanted.preonboarding.performanceSeat.presentation.controller;
+
+import com.wanted.preonboarding.core.domain.response.ResponseHandler;
+import com.wanted.preonboarding.performanceSeat.application.service.PerformanceSeatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/performanceSeat")
+public class PerformanceSeatController {
+
+    private final PerformanceSeatService performanceSeatService;
+
+    @GetMapping("/")
+    public ResponseHandler<Object> findReservableSeatByPerformanceId(@RequestParam UUID performanceId) {
+        return ResponseHandler.builder()
+                .statusCode(HttpStatus.OK)
+                .data(performanceSeatService.findReservableSeats(performanceId))
+                .build();
+    }
+}
