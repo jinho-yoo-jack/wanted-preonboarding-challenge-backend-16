fd3fdcf088081b6cad51756d89009177a16b86da
JangAJang <janghee5395@gmail.com>
2024-01-26 14:57:23 +0900

    refactor: @Param -> @RequestParam으로 어노테이션 수정

--- src/main/java/com/wanted/preonboarding/performance/presentation/controller/PerformanceController.java
+++ src/main/java/com/wanted/preonboarding/performance/presentation/controller/PerformanceController.java
@@ -3,10 +3,10 @@ package com.wanted.preonboarding.performance.presentation.controller;
 import com.wanted.preonboarding.core.domain.response.ResponseHandler;
 import com.wanted.preonboarding.performance.application.service.PerformanceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,10 +17,10 @@ public class PerformanceController {
     private final PerformanceService performanceService;
 
     @GetMapping("")
-    public ResponseHandler<Object> findAllPerformanceByReserveState(@Param("isReserve") String reserveState) {
+    public ResponseHandler<Object> findAllPerformanceByReserveState(@RequestParam String isReserve) {
         return ResponseHandler.builder()
                 .statusCode(HttpStatus.OK)
-                .data(performanceService.findPerformancesInReserveState(reserveState))
+                .data(performanceService.findPerformancesInReserveState(isReserve))
                 .build();
     }
 }
