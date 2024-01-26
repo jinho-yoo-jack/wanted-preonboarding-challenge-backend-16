5e5e3a6f2853e102c3cf9f06c6d5c52db827c815
JangAJang <janghee5395@gmail.com>
2024-01-26 15:42:37 +0900

    feat: 예약 대기와 관련된 이벤트 객체 구현

---
+++ src/main/java/com/wanted/preonboarding/reservation/domain/event/WaitToReserveEvent.java
@@ -0,0 +1,14 @@
+package com.wanted.preonboarding.reservation.domain.event;
+
+import com.wanted.preonboarding.common.model.SeatInfo;
+import com.wanted.preonboarding.performance.domain.entity.Performance;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class WaitToReserveEvent {
+
+    SeatInfo seatInfo;
+    Performance performance;
+}
