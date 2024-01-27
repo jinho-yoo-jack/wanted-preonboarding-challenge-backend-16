b3ac4e76e40f13f69f2fd0c98f4745e03e17eaec
bbbbooo <qlqlzbxkzl@naver.com>
2024-01-27 21:54:26 +0900

    remove : 사용하지 않는 임포트 제거함

--- src/main/java/com/wanted/preonboarding/ticketing/service/AlarmService.java
+++ src/main/java/com/wanted/preonboarding/ticketing/service/AlarmService.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
