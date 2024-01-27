2dbadab230b5601841dac51ec1de1c343795c721
leeedohyun <ldh19738@gmail.com>
2024-01-27 16:03:56 +0900

    feat: JpaAuditing configuration 등록

---
+++ src/main/java/com/wanted/preonboarding/ticket/configuration/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.wanted.preonboarding.ticket.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
