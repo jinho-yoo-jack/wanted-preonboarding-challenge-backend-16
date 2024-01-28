9b034eb25e2d65c66cc7910ab9d73d2c1b59b13f
Bombo <bombo96@chungwoon.ac.kr>
2024-01-28 13:46:10 +0900

    test: 예약 정보 응답에 누락된 필드를 추가하고 테스트를 수정

--- src/main/java/com/wanted/preonboarding/ticket/application/dto/response/ReserveResponse.java
+++ src/main/java/com/wanted/preonboarding/ticket/application/dto/response/ReserveResponse.java
@@ -2,6 +2,7 @@ package com.wanted.preonboarding.ticket.application.dto.response;
 
 import com.wanted.preonboarding.ticket.domain.entity.Performance;
 import com.wanted.preonboarding.ticket.domain.entity.Reservation;
+import com.wanted.preonboarding.ticket.infrastructure.dto.ReserveQueryResponse;
 import lombok.Builder;
 
 @Builder
@@ -10,6 +11,7 @@ public record ReserveResponse(
         String performanceName,
         int round,
         char line,
+        int gate,
         int seat,
         String reservationName,
         String reservationPhoneNumber
@@ -19,11 +21,25 @@ public record ReserveResponse(
         return ReserveResponse.builder()
                 .performanceId(performance.getId().toString())
                 .performanceName(performance.getName())
+                .gate(reservation.getGate())
                 .round(reservation.getRound())
                 .line(reservation.getLine())
                 .seat(reservation.getSeat())
                 .reservationName(reservation.getName())
                 .reservationPhoneNumber(reservation.getPhoneNumber())
+                .build();
+    }
+
+    public static ReserveResponse from(ReserveQueryResponse queryResponse) {
+        return ReserveResponse.builder()
+                .performanceId(queryResponse.performanceId().toString())
+                .performanceName(queryResponse.performanceName())
+                .gate(queryResponse.gate())
+                .round(queryResponse.round())
+                .line(queryResponse.line())
+                .seat(queryResponse.seat())
+                .reservationName(queryResponse.reservationName())
+                .reservationPhoneNumber(queryResponse.reservationPhoneNumber())
                 .build();
     }
 }
--- src/test/java/com/wanted/preonboarding/ticket/presentation/ReserveControllerTest.java
+++ src/test/java/com/wanted/preonboarding/ticket/presentation/ReserveControllerTest.java
@@ -3,10 +3,12 @@ package com.wanted.preonboarding.ticket.presentation;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wanted.preonboarding.ticket.application.ReservationService;
 import com.wanted.preonboarding.ticket.application.dto.request.CreateReserveServiceRequest;
+import com.wanted.preonboarding.ticket.application.dto.request.FindReserveServiceRequest;
 import com.wanted.preonboarding.ticket.application.dto.response.ReserveResponse;
 import com.wanted.preonboarding.ticket.application.exception.AlreadyReservedStateException;
 import com.wanted.preonboarding.ticket.application.validator.ReservationValidator;
 import com.wanted.preonboarding.ticket.presentation.dto.request.CreateReserveInfoRequest;
+import com.wanted.preonboarding.ticket.presentation.dto.request.FindReserveInfoRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +17,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -51,8 +55,10 @@ class ReserveControllerTest {
         ReserveResponse response = ReserveResponse.builder()
                 .performanceId(UUID.randomUUID().toString())
                 .performanceName("영웅")
+                .gate(1)
                 .round(1)
                 .line('a')
+                .seat(1)
                 .reservationName("원티드")
                 .reservationPhoneNumber("010-0000-1111")
                 .build();
@@ -70,6 +76,7 @@ class ReserveControllerTest {
                 .andExpect(jsonPath("$.data").exists())
                 .andExpect(jsonPath("$.data.performanceId").value(response.performanceId()))
                 .andExpect(jsonPath("$.data.performanceName").value(response.performanceName()))
+                .andExpect(jsonPath("$.data.gate").value(response.gate()))
                 .andExpect(jsonPath("$.data.round").value(response.round()))
                 .andExpect(jsonPath("$.data.line").value(String.valueOf(response.line())))
                 .andExpect(jsonPath("$.data.seat").value(response.seat()))
@@ -96,7 +103,6 @@ class ReserveControllerTest {
                         String.format(ReservationValidator.ALREADY_RESERVED_PERFORMANCE_MESSAGE_FORMAT, performanceId)));
 
         // when & then
-
         mockMvc.perform(post("/api/v1/reserves")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
@@ -105,6 +111,70 @@ class ReserveControllerTest {
                 .andExpect(jsonPath("$.statusCode").value(400))
                 .andExpect(jsonPath("$.message").value(
                         String.format(ReservationValidator.ALREADY_RESERVED_PERFORMANCE_MESSAGE_FORMAT, performanceId)))
+                .andExpect(jsonPath("$.serverTime").exists());
+    }
+
+    @DisplayName("고객의 예약 정보를 조회 할 수 있다.")
+    @Test
+    void findReservationInfoWithClient() throws Exception {
+        // given
+        String reservationName = "원티드";
+        String reservationPhoneNumber = "010-0000-1111";
+        String performanceId = UUID.randomUUID().toString();
+
+        FindReserveInfoRequest request = FindReserveInfoRequest.builder()
+                .reservationName(reservationName)
+                .reservationPhoneNumber(reservationPhoneNumber)
+                .build();
+
+        ReserveResponse response1 = ReserveResponse.builder()
+                .performanceId(performanceId)
+                .performanceName("영웅")
+                .round(1)
+                .line('a')
+                .seat(1)
+                .reservationName("원티드")
+                .reservationPhoneNumber("010-0000-1111")
+                .build();
+
+        ReserveResponse response2 = ReserveResponse.builder()
+                .performanceId(performanceId)
+                .performanceName("영웅")
+                .round(1)
+                .line('a')
+                .seat(2)
+                .reservationName("원티드")
+                .reservationPhoneNumber("010-0000-1111")
+                .build();
+
+        List<ReserveResponse> response = List.of(response1, response2);
+
+        given(reservationService.findReserve(any(FindReserveServiceRequest.class)))
+                .willReturn(response);
+
+        // when & then
+        mockMvc.perform(get("/api/v1/reserves")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .queryParam("reservationName", reservationName)
+                        .queryParam("reservationPhoneNumber", reservationPhoneNumber))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data[0].performanceId").value(response.get(0).performanceId()))
+                .andExpect(jsonPath("$.data[0].performanceName").value(response.get(0).performanceName()))
+                .andExpect(jsonPath("$.data[0].round").value(response.get(0).round()))
+                .andExpect(jsonPath("$.data[0].line").value(String.valueOf(response.get(0).line())))
+                .andExpect(jsonPath("$.data[0].seat").value(response.get(0).seat()))
+                .andExpect(jsonPath("$.data[0].reservationName").value(response.get(0).reservationName()))
+                .andExpect(jsonPath("$.data[0].reservationPhoneNumber").value(response.get(0).reservationPhoneNumber()))
+                .andExpect(jsonPath("$.data[1].performanceId").value(response.get(1).performanceId()))
+                .andExpect(jsonPath("$.data[1].performanceName").value(response.get(1).performanceName()))
+                .andExpect(jsonPath("$.data[1].round").value(response.get(1).round()))
+                .andExpect(jsonPath("$.data[1].line").value(String.valueOf(response.get(1).line())))
+                .andExpect(jsonPath("$.data[1].seat").value(response.get(1).seat()))
+                .andExpect(jsonPath("$.data[1].reservationName").value(response.get(1).reservationName()))
+                .andExpect(jsonPath("$.data[1].reservationPhoneNumber").value(response.get(1).reservationPhoneNumber()))
                 .andExpect(jsonPath("$.serverTime").exists());
     }
 }
