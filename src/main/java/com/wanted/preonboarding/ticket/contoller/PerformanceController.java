78d9fee5a294a8dafe0537e45a1cf46630b70ca1
leeedohyun <ldh19738@gmail.com>
2024-01-28 14:19:39 +0900

    feat: 공연 조회 컨트롤러 구현

---
+++ src/main/java/com/wanted/preonboarding/ticket/contoller/PerformanceController.java
@@ -0,0 +1,40 @@
+package com.wanted.preonboarding.ticket.contoller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wanted.preonboarding.core.domain.response.ResponseHandler;
+import com.wanted.preonboarding.ticket.service.PerformanceCheckResponseDto;
+import com.wanted.preonboarding.ticket.service.PerformanceService;
+import com.wanted.preonboarding.ticket.service.dto.request.PerformanceCheckRequestDto;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/performances")
+@Slf4j
+public class PerformanceController {
+
+    private final PerformanceService performanceService;
+
+    @GetMapping
+    public ResponseEntity<ResponseHandler<List<PerformanceCheckResponseDto>>> getPerformances(
+            @RequestBody final PerformanceCheckRequestDto request) {
+        log.info("getPerformances request: {}", request);
+        final List<PerformanceCheckResponseDto> performances = performanceService.getPerformances(request);
+        return ResponseEntity.ok()
+                .body(ResponseHandler.<List<PerformanceCheckResponseDto>>builder()
+                        .message("Get Performances Success")
+                        .statusCode(HttpStatus.OK)
+                        .data(performances)
+                        .build());
+    }
+}
