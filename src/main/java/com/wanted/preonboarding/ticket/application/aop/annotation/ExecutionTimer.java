59a3760a87a0aeaf317dcc87c577c6635e1ead2a
SeongHo5 <newfuring1123@gmail.com>
2024-01-28 23:18:45 +0900

    feat: Added AOP for measuring execution time

---
+++ src/main/java/com/wanted/preonboarding/ticket/application/aop/annotation/ExecutionTimer.java
@@ -0,0 +1,11 @@
+package com.wanted.preonboarding.ticket.application.aop.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExecutionTimer {
+}
