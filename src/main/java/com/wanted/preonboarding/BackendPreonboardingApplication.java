4de62b18fcd0bed32237f7643c0a79d62f771604
SeongHo5 <newfuring1123@gmail.com>
2024-01-29 12:59:01 +0900

    feat: Add time zone config

--- src/main/java/com/wanted/preonboarding/BackendPreonboardingApplication.java
+++ src/main/java/com/wanted/preonboarding/BackendPreonboardingApplication.java
@@ -1,8 +1,11 @@
 package com.wanted.preonboarding;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class BackendPreonboardingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendPreonboardingApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
