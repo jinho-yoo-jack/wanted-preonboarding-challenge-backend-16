0459459500c11b4e6751bbd4910b7c5feca2e5d5
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-01-29 23:05:01 +0900

    2024.01.29 commit
    1. Docker에 Mysql셋팅

--- src/main/resources/initdb/insert_test_data.sql
+++ src/main/resources/initdb/insert_test_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO `performance` (name, price, round, type, start_date, is_reserve)
-VALUES ('레베카', 100000, 1, 0, '2024-01-20 19:30:00', 'disable');
+VALUES ('Reveca', 100000, 1, 0, '2024-01-20 19:30:00', 'disable');
 
 INSERT INTO performance_seat_info VALUES
  (DEFAULT, (SELECT id FROM performance limit 1), 1, 1, 'A', 1, 'enable', DEFAULT, DEFAULT)
