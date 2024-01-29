7d7c16fc78d491ad3f9088098a76d6297e8656aa
KAispread <pain0121@naver.com>
2024-01-29 22:04:37 +0900

    feat: add discount table

---
+++ src/main/java/com/wanted/preonboarding/ticket/domain/discount/DiscountRepository.java
@@ -0,0 +1,7 @@
+package com.wanted.preonboarding.ticket.domain.discount;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscountRepository extends JpaRepository<Discount, Integer> {
+
+}
--- src/main/resources/initdb/create_schema.sql
+++ src/main/resources/initdb/create_schema.sql
@@ -44,3 +44,17 @@ CREATE TABLE IF NOT EXISTS `reservation`
     PRIMARY KEY (id),
     UNIQUE KEY reservation_round_row_seat (performance_id, round, `line`, seat)
 );
+
+CREATE TABLE IF NOT EXISTS `discount`
+(
+    `id`             INT(10)                NOT NULL AUTO_INCREMENT,
+    `performance_id` BINARY(16)             NOT NULL COMMENT '공연전시ID',
+    `name`           varchar(255)           NOT NULL COMMENT '할인명',
+    `type`           TINYINT                NOT NULL COMMENT '할인 종류',
+    `amount`         INT                    NOT NULL COMMENT '할인률 || 할인 금액',
+    `end_date`       DATETIME               NOT NULL COMMENT '유효기간',
+    `created_at`     DATETIME DEFAULT NOW() NOT NULL,
+    `updated_at`     DATETIME DEFAULT NOW() NOT NUll,
+    PRIMARY KEY (id),
+    UNIQUE KEY reservation_round_row_seat (`name`, `type`, `amount`, `end_date`)
+);
