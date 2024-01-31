1c9ceeeab96958a0347e6f9d0c78c8eae1c69fc1
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-02-01 03:12:30 +0900

    refactor: PerformTest 코드 정리

--- src/test/java/com/wanted/preonboarding/performance/domain/PerformTest.java
+++ src/test/java/com/wanted/preonboarding/performance/domain/PerformTest.java
@@ -1,18 +1,7 @@
 package com.wanted.preonboarding.performance.domain;
 
-import com.wanted.preonboarding.performance.ReservationRequestFactory;
-import com.wanted.preonboarding.performance.domain.creator.PerformCreator;
-import com.wanted.preonboarding.reservation.framwork.presentation.dto.ReservationRequest;
 import org.junit.jupiter.api.DisplayName;
 
 @DisplayName("도메인: 공연 및 전시 상영 - Perform")
 public class PerformTest {
-
-	private final PerformCreator performCreator = new PerformCreator();
-	private final Perform perform = performCreator.getPerform();
-	private final ReservationRequestFactory factory = new ReservationRequestFactory();
-	private final Performance performance = perform.getPerformance();
-	private final ReservationRequest reservationRequest = factory.create(performance.getId());;
-
-
 }
