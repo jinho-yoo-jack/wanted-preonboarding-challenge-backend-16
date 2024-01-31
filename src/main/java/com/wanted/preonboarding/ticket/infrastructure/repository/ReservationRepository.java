08a330909e2a322f9be401eeaa33430c1a23e17a
jsoyun <syusmm233@gmail.com>
2024-01-31 23:53:05 +0900

    Add: ReservationRepository

--- src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/ReservationRepository.java
+++ src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/ReservationRepository.java
@@ -1,12 +1,15 @@
 package com.wanted.preonboarding.ticket.infrastructure.repository;
 
-import com.wanted.preonboarding.ticket.domain.entity.Performance;
 import com.wanted.preonboarding.ticket.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
-    Reservation findByNameAndPhoneNumber(String name, String phoneNumber);
+    Optional<Reservation> findByNameAndPhoneNumber(String name, String phoneNumber);
+
+    void deleteByPerformanceIdAndNameAndPhoneNumberAndRound(
+            UUID performanceId,String name,String phoneNumber, int round
+    );
 }
