f158e6446dc0b8b73212c72653f4838a4c048ccc
youngpar <youngseo321@gmail.com>
2024-01-31 19:54:30 +0900

    Repository

---
+++ src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.wanted.preonboarding.ticket.infrastructure.repository;
+
+import com.wanted.preonboarding.ticket.domain.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * {@link UserInfo}를 제어하기 위한 JPA 레포지토리 인터페이스입니다.
+ * 유저 정보에 접근하기 위한 CRUD 연산을 제공합니다.
+ */
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+}
