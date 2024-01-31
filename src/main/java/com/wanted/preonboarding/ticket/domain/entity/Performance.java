f31d64f9b84514320faaa99bfa5526efdabdeb4a
jsoyun <syusmm233@gmail.com>
2024-01-31 23:36:27 +0900

    Add: Performance Entity

--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/Performance.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/Performance.java
@@ -1,5 +1,6 @@
 package com.wanted.preonboarding.ticket.domain.entity;
 
+import com.wanted.preonboarding.ticket.domain.dto.PerformanceType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,8 +19,8 @@ import java.util.UUID;
 @AllArgsConstructor
 public class Performance {
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+//    @GeneratedValue(generator = "uuid2")
+//    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)")
     private UUID id;
     @Column(nullable = false)
@@ -29,10 +30,20 @@ public class Performance {
     @Column(nullable = false)
     private int round;
     @Column(nullable = false)
-    private int type;
+    private int type;  //공연 타입 PerformanceType
     @Column(nullable = false)
-    private Date start_date;
+    private Date startDate;
     @Column(nullable = false, name = "is_reserve", columnDefinition = "varchar default 'disable'")
     private String isReserve;
+
+//    public PerformanceType getPerformanceType(){
+//        return PerformanceType.values()[type];
+//
+//    }
+//
+//    public void setPerformanceType(PerformanceType performanceType){
+//        this.type = performanceType.getCategory();
+//
+//    }
 
 }
