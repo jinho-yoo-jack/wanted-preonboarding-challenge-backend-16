7def22adb174f1110d642edea3c32a7cef4af1ac
jsoyun <syusmm233@gmail.com>
2024-01-31 23:54:08 +0900

    Add: findByIdPerformanceInfoList

--- src/test/java/com/wanted/preonboarding/ticket/application/TicketSellerTest.java
+++ src/test/java/com/wanted/preonboarding/ticket/application/TicketSellerTest.java
@@ -1,11 +1,17 @@
 package com.wanted.preonboarding.ticket.application;
 
+import com.wanted.preonboarding.ticket.domain.entity.Performance;
 import com.wanted.preonboarding.ticket.infrastructure.repository.PerformanceRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -15,8 +21,18 @@ public class TicketSellerTest {
 
     @Test
     public void getAllPerformanceInfoList() {
-        System.out.println("RESULT => " + performanceRepository.findAll());
+        List<Performance> performances = performanceRepository.findAll();
+        for (Performance performance : performances) {
+            System.out.println("performance = " + performance.getId());
+        }
+    }
 
+    @Test
+    public void findByIdPerformanceInfoList() {
+        Performance performance = performanceRepository.findById(UUID.fromString("4438a3e6-b01c-11ee-9426-0242ac180002"))
+                .orElseThrow(EntityNotFoundException::new);
+
+        System.out.println("performance.getId() = " + performance.getId());
     }
 
 }
