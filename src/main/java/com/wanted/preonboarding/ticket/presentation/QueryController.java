580e4acc95003daef739c19fae2b0fd9f8573d59
jsoyun <syusmm233@gmail.com>
2024-01-31 23:38:55 +0900

    Add: addPerformance api

--- src/main/java/com/wanted/preonboarding/ticket/presentation/QueryController.java
+++ src/main/java/com/wanted/preonboarding/ticket/presentation/QueryController.java
@@ -2,13 +2,15 @@ package com.wanted.preonboarding.ticket.presentation;
 
 import com.wanted.preonboarding.core.domain.response.ResponseHandler;
 import com.wanted.preonboarding.ticket.application.TicketSeller;
+import com.wanted.preonboarding.ticket.domain.dto.CreatePerformance;
+import com.wanted.preonboarding.ticket.domain.dto.PerformanceIdRequest;
 import com.wanted.preonboarding.ticket.domain.dto.PerformanceInfo;
+import com.wanted.preonboarding.ticket.domain.entity.Performance;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
@@ -18,16 +20,31 @@ import java.util.List;
 public class QueryController {
     private final TicketSeller ticketSeller;
 
+
+    //TODO: 예약 가능한 공연 목록 조회
     @GetMapping("/all/performance")
     public ResponseEntity<ResponseHandler<List<PerformanceInfo>>> getAllPerformanceInfoList() {
         System.out.println("getAllPerformanceInfoList");
+
+        List<PerformanceInfo> allPerformanceInfoList = ticketSeller.getAllPerformanceInfoList();
+        allPerformanceInfoList.stream().forEach(e -> e.getPerformanceId());
+
         return ResponseEntity
-            .ok()
-            .body(ResponseHandler.<List<PerformanceInfo>>builder()
-                .message("Success")
-                .statusCode(HttpStatus.OK)
-                .data(ticketSeller.getAllPerformanceInfoList())
-                .build()
-            );
+                .ok()
+                .body(ResponseHandler.<List<PerformanceInfo>>builder()
+                        .message("Success")
+                        .statusCode(HttpStatus.OK)
+                        .data(ticketSeller.getAllPerformanceInfoList())
+                        .build()
+                );
     }
+
+    @PostMapping("/performance")
+    public ResponseEntity<Boolean> putPerformanceInfo(@RequestBody CreatePerformance createPerformance) {
+        boolean result = ticketSeller.addPerformance(createPerformance);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+
+    }
+
+
 }
