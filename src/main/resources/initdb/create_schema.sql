c2c1acfb8801344fc2782e41390412080c4b1ee2
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-02-01 04:27:13 +0900

    fix: create_schema.sql 수정

--- src/main/resources/initdb/create_schema.sql
+++ src/main/resources/initdb/create_schema.sql
@@ -1,46 +1,98 @@
--- schema.sql
-CREATE TABLE IF NOT EXISTS `performance`
+CREATE TABLE amount_discount_policy
 (
-    `id`         BINARY(16) default (uuid_to_bin(uuid())) NOT NULL COMMENT '공연/전시 ID',
-    `name`       varchar(255)                             NOT NULL COMMENT '공연/전시 이름',
-    `price`      INT                                      NOT NULL COMMENT '가격',
-    `round`      INT                                      NOT NULL COMMENT '회차',
-    `type`       INT                                      NOT NULL COMMENT 'NONE, CONCERT, EXHIBITION',
-    `start_date` datetime                                 NOT NULL COMMENT '공연 일시',
-    `is_reserve` varchar(255)                             NOT NULL DEFAULT 'disable',
-    `created_at` DATETIME   DEFAULT NOW()                 NOT NULL,
-    `updated_at` DATETIME   DEFAULT NOW()                 NOT NUll,
-    PRIMARY KEY (id),
-    UNIQUE KEY (id, round)
-);
+    amount INT    NOT NULL,
+    id     BIGINT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `performance_seat_info`
+CREATE TABLE discount_policy
 (
-    `id`             INT(10)                NOT NULL AUTO_INCREMENT,
-    `performance_id` BINARY(16)             NOT NULL COMMENT '공연전시ID',
-    `round`          INT                    NOT NULL COMMENT '회차(FK)',
-    `gate`           INT                    NOT NULL COMMENT '입장 게이트',
-    `line`           CHAR(2)                NOT NULL COMMENT '좌석 열',
-    `seat`           INT                    NOT NULL COMMENT '좌석 행',
-    `is_reserve`     varchar(255)           NOT NULL default 'enable',
-    `created_at`     DATETIME DEFAULT NOW() NOT NULL,
-    `updated_at`     DATETIME DEFAULT NOW() NOT NUll,
-    PRIMARY KEY (id),
-    UNIQUE KEY performance_seat_info_unique (performance_id, round, `line`, seat)
-);
+    id            BIGINT      NOT NULL AUTO_INCREMENT,
+    discount_type VARCHAR(31) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `reservation`
+CREATE TABLE none_discount_policy
 (
-    `id`             INT(10)                NOT NULL AUTO_INCREMENT,
-    `performance_id` BINARY(16)             NOT NULL COMMENT '공연전시ID',
-    `name`           varchar(255)           NOT NULL COMMENT '예약자명',
-    `phone_number`   varchar(255)           NOT NULL COMMENT '예약자 휴대전화 번호',
-    `round`          INT                    NOT NULL COMMENT '회차(FK)',
-    `gate`           INT                    NOT NULL COMMENT '입장 게이트',
-    `line`           CHAR                   NOT NULL COMMENT '좌석 열',
-    `seat`           INT                    NOT NULL COMMENT '좌석 행',
-    `created_at`     DATETIME DEFAULT NOW() NOT NULL,
-    `updated_at`     DATETIME DEFAULT NOW() NOT NUll,
+    id BIGINT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE percent_discount_policy
+(
+    percent FLOAT(53) NOT NULL,
+    id      BIGINT    NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE perform
+(
+    gate                  INT        NOT NULL,
+    reservation_available BIT        NOT NULL,
+    round                 INT        NOT NULL,
+    start_date            DATE       NOT NULL,
+    id                    BINARY(16) NOT NULL,
+    performance_id        BINARY(16) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE performance
+(
+    price              INT                                    NOT NULL,
+    discount_policy_id BIGINT,
+    id                 BINARY(16)                             NOT NULL,
+    name               VARCHAR(255)                           NOT NULL,
+    type               ENUM ('NONE', 'CONCERT', 'EXHIBITION') NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE KEY reservation_round_row_seat (performance_id, round, `line`, seat)
-);
+    CONSTRAINT UK_nuypy298tmuksi6s328r8231c UNIQUE (discount_policy_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE reservation
+(
+    id           BINARY(16)   NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE reservation_reserve_item_list
+(
+    fee                INT          NOT NULL,
+    line               CHAR(1)      NOT NULL,
+    purchase_date      DATE         NOT NULL,
+    round              INT          NOT NULL,
+    seat               INT          NOT NULL,
+    id                 BINARY(16)   NOT NULL,
+    no                 BINARY(16)   NOT NULL,
+    reservation_id     BINARY(16)   NOT NULL,
+    name               VARCHAR(255) NOT NULL,
+    reservation_status ENUM ('RESERVE', 'CANCEL'),
+    CONSTRAINT FKfgk3770kws30j6p3orvw25ks7 FOREIGN KEY (reservation_id) REFERENCES reservation (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE reservation_cancel_subscribe
+(
+    id        BINARY(16)   NOT NULL,
+    perfor_id BINARY(16),
+    user_id   VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT FKhe8dp5283hpdmadyvold8xo8i FOREIGN KEY (perfor_id) REFERENCES perform (id)
+) ENGINE = InnoDB;
+
+ALTER TABLE amount_discount_policy
+    ADD CONSTRAINT FK2q0sacr6mjl9o5d6pmhr70xao FOREIGN KEY (id) REFERENCES discount_policy (id);
+
+ALTER TABLE none_discount_policy
+    ADD CONSTRAINT FK46ywd4acojrwuc30q5urngifl FOREIGN KEY (id) REFERENCES discount_policy (id);
+
+ALTER TABLE percent_discount_policy
+    ADD CONSTRAINT FK3w28nl5i2tpob3xx1xcro5g0w FOREIGN KEY (id) REFERENCES discount_policy (id);
+
+ALTER TABLE perform
+    ADD CONSTRAINT FKnkpoc1jbq41o850i7qvt584wx FOREIGN KEY (performance_id) REFERENCES performance (id);
+
+ALTER TABLE performance
+    ADD CONSTRAINT FK7ticpplal5ebcktyird7kw7x6 FOREIGN KEY (discount_policy_id) REFERENCES discount_policy (id);
+
+ALTER TABLE reservation_cancel_subscribe
+    ADD CONSTRAINT FKhe8dp5283hpdmadyvold8xo8i FOREIGN KEY (perfor_id) REFERENCES perform (id);
