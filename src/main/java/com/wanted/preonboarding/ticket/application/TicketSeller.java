64c09934214c91241933019939be21d042005293
jsoyun <syusmm233@gmail.com>
2024-01-31 23:41:15 +0900

    Add: addPerformance Transaction method

--- src/main/java/com/wanted/preonboarding/ticket/application/TicketSeller.java
+++ src/main/java/com/wanted/preonboarding/ticket/application/TicketSeller.java
@@ -1,17 +1,14 @@
 package com.wanted.preonboarding.ticket.application;
 
-import com.wanted.preonboarding.ticket.domain.dto.PerformanceInfo;
-import com.wanted.preonboarding.ticket.domain.dto.ReserveInfo;
+import com.wanted.preonboarding.ticket.domain.dto.*;
 import com.wanted.preonboarding.ticket.domain.entity.Performance;
-import com.wanted.preonboarding.ticket.domain.entity.Reservation;
 import com.wanted.preonboarding.ticket.infrastructure.repository.PerformanceRepository;
 import com.wanted.preonboarding.ticket.infrastructure.repository.ReservationRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -21,33 +18,25 @@ public class TicketSeller {
     private final ReservationRepository reservationRepository;
     private long totalAmount = 0L;
 
-    public List<PerformanceInfo> getAllPerformanceInfoList() {
-        return performanceRepository.findByIsReserve("enable")
-            .stream()
-            .map(PerformanceInfo::of)
-            .toList();
-    }
+    @Transactional
+    public boolean addPerformance(CreatePerformance createPerformance) {
+        UUID uuid = UUID.randomUUID();
 
-    public PerformanceInfo getPerformanceInfoDetail(String name) {
-        return PerformanceInfo.of(performanceRepository.findByName(name));
-    }
+        Performance performance = Performance.builder().
+                id(uuid)
+                .name(createPerformance.getName())
+                .price(createPerformance.getPrice())
+                .round(createPerformance.getRound())
+                .type(createPerformance.getType())
+                .startDate(createPerformance.getStart_date())
+                .isReserve(createPerformance.getIsReserve())
+                .build();
 
-    public boolean reserve(ReserveInfo reserveInfo) {
-        log.info("reserveInfo ID => {}", reserveInfo.getPerformanceId());
-        Performance info = performanceRepository.findById(reserveInfo.getPerformanceId())
-            .orElseThrow(EntityNotFoundException::new);
-        String enableReserve = info.getIsReserve();
-        if (enableReserve.equalsIgnoreCase("enable")) {
-            // 1. 결제
-            int price = info.getPrice();
-            reserveInfo.setAmount(reserveInfo.getAmount() - price);
-            // 2. 예매 진행
-            reservationRepository.save(Reservation.of(reserveInfo));
-            return true;
+        performanceRepository.save(performance);
 
-        } else {
-            return false;
-        }
+
+        return true;
+
     }
 
 }
