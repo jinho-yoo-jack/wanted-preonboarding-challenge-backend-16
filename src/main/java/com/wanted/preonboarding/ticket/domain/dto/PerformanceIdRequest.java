57384b578dacf1dc356c330112ba5314855e6916
jsoyun <syusmm233@gmail.com>
2024-01-31 23:48:38 +0900

    Add: PerformanceIdRequest

---
+++ src/main/java/com/wanted/preonboarding/ticket/domain/dto/PerformanceIdRequest.java
@@ -0,0 +1,23 @@
+package com.wanted.preonboarding.ticket.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.util.UUID;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PerformanceIdRequest {
+
+    private String performanceName;
+    private int performanceType;
+    private int performanceRound;
+    private Date startDate;
+    private String isReserve;
+
+}
