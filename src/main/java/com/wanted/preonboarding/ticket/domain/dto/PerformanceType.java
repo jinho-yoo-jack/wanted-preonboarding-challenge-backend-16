080afc7b341167933fb747fe66117e82d0cb81fb
jsoyun <syusmm233@gmail.com>
2024-01-31 23:49:47 +0900

    Add: PerformanceSeatInfo, PerformanceType (unused)

--- src/main/java/com/wanted/preonboarding/ticket/domain/dto/PerformanceType.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/dto/PerformanceType.java
@@ -1,21 +1,25 @@
 package com.wanted.preonboarding.ticket.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 public enum PerformanceType {
-    NONE(0),
-    CONCERT(1),
-    EXHIBITION(2);
 
+    NONE(0, "None"),
+    CONCERT(1, "Concert"),
+    EXHIBITION(2, "Exhibition");
+
+    @Getter
     private final int category;
+    private final String description;
 
-    PerformanceType(int category) {
+    PerformanceType(int category ,String description) {
+
         this.category = category;
+        this.description = description;
     }
 
-    public int getCategory() {
-        return category;
-    }
+
 
 }
--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/PerformanceSeatInfo.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/PerformanceSeatInfo.java
@@ -1,4 +1,24 @@
 package com.wanted.preonboarding.ticket.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+//공연좌석 저장소
+@Entity
+@Table(name = "performance_seat_info")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PerformanceSeatInfo {
+
+    @Id
+    private Long id;
+    String section;
+
 }
