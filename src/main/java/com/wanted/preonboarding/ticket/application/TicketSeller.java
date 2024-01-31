01399417e900206510e87d8fb45958b42e4a167a
jsoyun <syusmm233@gmail.com>
2024-01-31 23:45:39 +0900

    Add: getAllPerformanceInfoList method

--- src/main/java/com/wanted/preonboarding/ticket/application/TicketSeller.java
+++ src/main/java/com/wanted/preonboarding/ticket/application/TicketSeller.java
@@ -2,12 +2,18 @@ package com.wanted.preonboarding.ticket.application;
 
 import com.wanted.preonboarding.ticket.domain.dto.*;
 import com.wanted.preonboarding.ticket.domain.entity.Performance;
+import com.wanted.preonboarding.ticket.domain.entity.Reservation;
 import com.wanted.preonboarding.ticket.infrastructure.repository.PerformanceRepository;
 import com.wanted.preonboarding.ticket.infrastructure.repository.ReservationRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
@@ -38,5 +44,16 @@ public class TicketSeller {
         return true;
 
     }
+
+    public List<PerformanceInfo> getAllPerformanceInfoList() {
+        //예약 가능한 공연리스트 가져옴
+        return performanceRepository.findByIsReserve("enable")
+                .stream()
+                .map(PerformanceInfo::of)
+                .toList();
+    }
+
+
+
 
 }
