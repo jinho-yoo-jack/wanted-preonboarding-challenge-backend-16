3d5e2cd3302d6189359a8daf9e18c7eefb6ca0e6
jsoyun <syusmm233@gmail.com>
2024-01-31 23:51:40 +0900

    Add: ReserveInfo

--- src/main/java/com/wanted/preonboarding/ticket/domain/dto/ReserveInfo.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/dto/ReserveInfo.java
@@ -1,23 +1,33 @@
 package com.wanted.preonboarding.ticket.domain.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import java.util.Date;
 import java.util.UUID;
+
 
 @Getter
 @Setter
 @Builder
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+//예약 신청
 public class ReserveInfo {
     // 공연 및 전시 정보 + 예약자 정보
     private UUID performanceId;
     private String reservationName;
     private String reservationPhoneNumber;
-    private String reservationStatus; // 예약; 취소;
-    private long amount;
+    private String reservationStatus; //예약 상태: 요청 Requested 대기 Pending 승인 Approved 거절 Rejected
+    private long amount; //결제 가능한 금액 (잔고)
     private int round;
     private char line;
     private int seat;
+
+    public void setReserveInfo(String status){
+        this.reservationStatus = status ;
+
+    }
+
+
 }
