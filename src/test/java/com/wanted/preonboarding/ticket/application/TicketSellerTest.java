59e97f7aa124556133e2c6e772ed53c6624042d2
jsoyun <syusmm233@gmail.com>
2024-02-01 16:49:36 +0900

    Add: getPerformanceId test, log

--- src/test/java/com/wanted/preonboarding/ticket/application/TicketSellerTest.java
+++ src/test/java/com/wanted/preonboarding/ticket/application/TicketSellerTest.java
@@ -4,6 +4,7 @@ import com.wanted.preonboarding.ticket.domain.dto.PerformanceIdRequest;
 import com.wanted.preonboarding.ticket.domain.entity.Performance;
 import com.wanted.preonboarding.ticket.infrastructure.repository.PerformanceRepository;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +15,14 @@ import java.sql.Date;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class TicketSellerTest {
     @Autowired
     private PerformanceRepository performanceRepository;
 
+    //생성시 UUID가 존재하면 추가되지 않도록 pk로
     @Test
     public void addPerformance() {
         UUID uuid = UUID.randomUUID();
@@ -43,6 +46,7 @@ public class TicketSellerTest {
         List<Performance> performances = performanceRepository.findAll();
         for (Performance performance : performances) {
             System.out.println("performance = " + performance.getId());
+
         }
     }
 
@@ -51,7 +55,28 @@ public class TicketSellerTest {
         Performance performance = performanceRepository.findById(UUID.fromString("4438a3e6-b01c-11ee-9426-0242ac180002"))
                 .orElseThrow(EntityNotFoundException::new);
 
-        System.out.println("performance.getId() = " + performance.getId());
+        log.info("performanceId=", performance.getId());
+    }
+
+
+    @Test
+    public void getPerformanceId() {
+        PerformanceIdRequest performanceIdRequest = PerformanceIdRequest.builder()
+                .performanceName("브루노마스")
+                .performanceType(1)
+                .performanceRound(1)
+                .startDate(Date.valueOf("2024-01-31"))
+                .build();
+
+        Performance performance = performanceRepository.findByNameAndTypeAndRoundAndStartDate(
+                performanceIdRequest.getPerformanceName(),
+                performanceIdRequest.getPerformanceType(),
+                performanceIdRequest.getPerformanceRound(),
+                performanceIdRequest.getStartDate()
+        );
+
+        log.info("performanceId=", performance.getId());
+
     }
 
 }
