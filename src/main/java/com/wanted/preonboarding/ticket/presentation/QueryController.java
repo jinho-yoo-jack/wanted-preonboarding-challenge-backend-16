1d807bb06e7d35ef133bac5abda13714d89e6a8f
jsoyun <syusmm233@gmail.com>
2024-02-01 23:34:01 +0900

    Fix: performance return value

--- src/main/java/com/wanted/preonboarding/ticket/presentation/QueryController.java
+++ src/main/java/com/wanted/preonboarding/ticket/presentation/QueryController.java
@@ -1,16 +1,13 @@
 package com.wanted.preonboarding.ticket.presentation;
 
-import com.wanted.preonboarding.core.domain.response.ResponseHandler;
 import com.wanted.preonboarding.ticket.application.TicketSeller;
 import com.wanted.preonboarding.ticket.domain.dto.CreatePerformance;
 import com.wanted.preonboarding.ticket.domain.dto.PerformanceIdRequest;
 import com.wanted.preonboarding.ticket.domain.dto.PerformanceInfo;
-import com.wanted.preonboarding.ticket.domain.entity.Performance;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.List;
 import java.util.UUID;
@@ -24,33 +21,25 @@ public class QueryController {
 
     //TODO: 예약 가능한 공연 목록 조회
     @GetMapping("/all/performance")
-    public ResponseEntity<ResponseHandler<List<PerformanceInfo>>> getAllPerformanceInfoList() {
+    public ResponseEntity<List<PerformanceInfo>> getAllPerformanceInfoList() {
         List<PerformanceInfo> allPerformanceInfoList = ticketSeller.getAllPerformanceInfoList();
-        allPerformanceInfoList.stream().forEach(e -> e.getPerformanceId());
-
-        return ResponseEntity
-                .ok()
-                .body(ResponseHandler.<List<PerformanceInfo>>builder()
-                        .message("Success")
-                        .statusCode(HttpStatus.OK)
-                        .data(ticketSeller.getAllPerformanceInfoList())
-                        .build()
-                );
+        return new ResponseEntity<>(allPerformanceInfoList, HttpStatus.OK);
     }
 
 
     @PostMapping("/performance")
-    public ResponseEntity<Boolean> putPerformanceInfo(@RequestBody CreatePerformance createPerformance) {
-        boolean result = ticketSeller.addPerformance(createPerformance);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    public ResponseEntity<PerformanceInfo> putPerformanceInfo(@RequestBody CreatePerformance createPerformance) {
+        PerformanceInfo performanceInfo = ticketSeller.addPerformance(createPerformance);
+        return new ResponseEntity<>(performanceInfo, HttpStatus.OK);
 
     }
+
 
     //TODO: 예약가능 공연 상세정보조회
     @PostMapping("/specificPerformance")
     public ResponseEntity<UUID> getPerformanceId(@RequestBody PerformanceIdRequest performanceIdRequest) {
         UUID performanceId = ticketSeller.getPerformanceId(performanceIdRequest);
-        return new ResponseEntity<>(performanceId,HttpStatus.OK);
+        return new ResponseEntity<>(performanceId, HttpStatus.OK);
     }
 
 
