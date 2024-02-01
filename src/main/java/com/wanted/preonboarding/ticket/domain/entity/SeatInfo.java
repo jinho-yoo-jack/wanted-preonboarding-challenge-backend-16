dd2ceb3e9782e898d76058c435f1a74ad4f98653
sunyesle <sunyesle@gmail.com>
2024-02-01 22:10:32 +0900

    feat: VO 클래스 equals, hashCode 메서드 오버라이딩

--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/SeatInfo.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/SeatInfo.java
@@ -7,18 +7,33 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Embeddable
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SeatInfo {
-
     private Integer round;
-
     private Integer gate;
-
     private String line;
-
     private Integer seat;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SeatInfo seatInfo = (SeatInfo) o;
+        return round.equals(seatInfo.getRound()) && gate.equals(seatInfo.getGate()) && line.equals(seatInfo.getLine()) && seat.equals(seatInfo.getSeat());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(round, gate, line, seat);
+    }
 }
--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/UserInfo.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/UserInfo.java
@@ -7,14 +7,31 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Embeddable
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInfo {
-
     private String name;
-
     private String phoneNumber;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserInfo userInfo = (UserInfo) o;
+        return name.equals(userInfo.getName()) && phoneNumber.equals(userInfo.getPhoneNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phoneNumber);
+    }
 }
