b090c2094ca5e3248a560c75f1db01f7dfa56b39
brightGarden02 <rreone2017@gmail.com>
2024-02-02 19:19:09 +0900

    refactor: 미사용 @EnableScheduling 제거

--- src/main/java/com/wanted/preonboarding/BackendPreonboardingApplication.java
+++ src/main/java/com/wanted/preonboarding/BackendPreonboardingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class BackendPreonboardingApplication {
