3b67d64cdb3043b79cfcd240337e132ff87cb4e6
KAispread <pain0121@naver.com>
2024-02-02 18:26:01 +0900

    chore: add querydsl dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,14 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // QueryDSL
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    implementation("com.querydsl:querydsl-sql:5.0.0")
+    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.mysql:mysql-connector-j:8.0.33")
@@ -45,4 +53,26 @@ tasks.bootJar {
 
 tasks.bootBuildImage {
     imageName = "wanted/preonboarding-backend"
+}
+
+// Querydsl
+val querydslDir = "$buildDir/generated/querydsl"
+
+sourceSets {
+    main {
+        java.srcDirs("src/main/java", querydslDir)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.generatedSourceOutputDirectory.file(querydslDir)
+    options.compilerArgs.add("-Aquerydsl.generatedAnnotationClass=javax.annotation.Generated")
+}
+
+tasks {
+    named("clean") {
+        doLast {
+            file(querydslDir).deleteRecursively()
+        }
+    }
 }
