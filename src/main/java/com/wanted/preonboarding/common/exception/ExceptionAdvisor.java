1722af2feb0c7d53872d7c5fbb448c1cf91b8307
soo-in-lee <dltndls321@gmail.com>
2024-02-03 00:48:00 +0900

    익셉션 주석 추가 및 httpMessageNotReadableException,noResourceFoundException 추가

--- src/main/java/com/wanted/preonboarding/common/exception/ExceptionAdvisor.java
+++ src/main/java/com/wanted/preonboarding/common/exception/ExceptionAdvisor.java
@@ -4,13 +4,15 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 /**
- * 익셉션 별
+ * 익셉션 별 코드,메세지 관리
  * 
  */
 @ControllerAdvice
@@ -20,8 +22,44 @@ public class ExceptionAdvisor {
 	@Autowired
 	private ExceptionService exceptionService;
 	
+    /**
+     * 적정 리퀘스트 파라미터가 존재하지 않을시 발생
+	 * errorcode : 8401
+     * @param methodArgumentNotValidException
+     * @return
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,Object>> processValidationError(MethodArgumentNotValidException exception) {
-        return ResponseEntity.badRequest().body(exceptionService.selectMethodArgumentNotValidExceptionMessage(exception));
+    public ResponseEntity<Map<String,Object>> processValidationError(MethodArgumentNotValidException methodArgumentNotValidException) {
+        return ResponseEntity.badRequest().body(exceptionService.selectMethodArgumentNotValidExceptionMessage(methodArgumentNotValidException));
     }
+    /**
+     * 메세지 형태가 이상하거나 존재하지 않을시 발생
+	 * errorcode : 8402
+     * @param httpMessageNotReadableException
+     * @return
+     */
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<Map<String,Object>> processHttpMessageNotReadableError(HttpMessageNotReadableException httpMessageNotReadableException) {
+		return ResponseEntity.badRequest().body(exceptionService.selectHttpMessageNotReadableExceptionMessage(httpMessageNotReadableException));
+	}
+    /**
+     * 잘못된 주소로 요청을 보낼경우 발생
+	 * errorcode : 8403
+     * @param noResourceFoundException
+     * @return
+     */
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Map<String,Object>> processNoResourceFoundError(NoResourceFoundException noResourceFoundException) {
+    	return ResponseEntity.badRequest().body(exceptionService.selectNoResourceFoundExceptionMessage(noResourceFoundException));
+    }
+    /**
+     * 기본 오류 메세지
+	 * errorcode : 8400
+     * @param exception
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+	public ResponseEntity<Map<String,Object>> processDefaultError(Exception exception) {
+		return ResponseEntity.badRequest().body(exceptionService.selectDefaultExceiptionMessage(exception));
+	}
 }
--- src/main/java/com/wanted/preonboarding/common/exception/ExceptionService.java
+++ src/main/java/com/wanted/preonboarding/common/exception/ExceptionService.java
@@ -3,33 +3,32 @@ package com.wanted.preonboarding.common.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Service
 public class ExceptionService {
-	
+	/** =======================에러 코드 시작====================================*/
 	private final int  DefaultExceiptionErrorCode = 8400; 
 	private final int  MethodArgumentNotValidExceptionErrorCode = 8401; 
-	
-	
-	public Map<String,Object> selectDefaultExceiptionMessage(Exception exception){
-		Map<String, Object> errorMap = new HashMap<String, Object>();
-		
-		return errorMap;
-	}
-	
+	private final int  HttpMessageNotReadableExceptionErrorCode = 8402; 
+	private final int  NoResourceFoundExceptionErrorCode = 8403; 
+	/** =======================에러 코드 종료====================================*/
+
 	/**
 	 * 적정 리퀘스트 파라미터가 존재하지 않을시 발생 오류 메세지 생성
-	 * @param exception
+	 * errorcode : 8401
+	 * @param methodArgumentNotValidException
 	 * @return
 	 */
-	public Map<String,Object> selectMethodArgumentNotValidExceptionMessage(MethodArgumentNotValidException exception){
+	public Map<String,Object> selectMethodArgumentNotValidExceptionMessage(MethodArgumentNotValidException methodArgumentNotValidException){
 		Map<String, Object> errorMap = new HashMap<String, Object>();
 		
-		BindingResult bindingResult = exception.getBindingResult();
+		BindingResult bindingResult = methodArgumentNotValidException.getBindingResult();
 		StringBuffer sb = new StringBuffer();
 		
 		for (FieldError fieldError : bindingResult.getFieldErrors()) {
@@ -53,16 +52,49 @@ public class ExceptionService {
 	}
 	
 	/**
-	 * 기본 오류 메세지 생성
-	 * @param exception
+	 * 메세지 형태가 이상하거나 존재하지 않을시 발생 오류 메세지 생성
+	 * errorcode : 8402
+	 * @param httpMessageNotReadableException
 	 * @return
 	 */
-	public Map<String,Object> selectDefaultExceiptionMessage(MethodArgumentNotValidException exception){
+	public Map<String,Object> selectHttpMessageNotReadableExceptionMessage(HttpMessageNotReadableException httpMessageNotReadableException) {
 		Map<String, Object> errorMap = new HashMap<String, Object>();
 		
-		errorMap.put("errorCode", DefaultExceiptionErrorCode);
-		errorMap.put("message", "알수 없는 이유의 오류가 발생했습니다.");
+		errorMap.put("errorCode", HttpMessageNotReadableExceptionErrorCode);
+		errorMap.put("message", "API 메세지 형태를 확인해 주세요.");
 		
 		return errorMap;
 	}
+	
+	/**
+	 * 잘못된 주소로 요청을 보낼경우 발생 오류 메세지 생성
+	 * errorcode : 8403
+	 * @param noResourceFoundException
+	 * @return
+	 */
+	public Map<String,Object> selectNoResourceFoundExceptionMessage(NoResourceFoundException noResourceFoundException) {
+		Map<String, Object> errorMap = new HashMap<String, Object>();
+		
+		errorMap.put("errorCode", NoResourceFoundExceptionErrorCode);
+		errorMap.put("message", "API 주소를 확인해 주세요.");
+		
+		return errorMap;
+	}
+	
+	/**
+	 * 기본 오류 메세지 생성
+	 * errorcode : 8400
+	 * @param defaultException
+	 * @return
+	 */
+	public Map<String,Object> selectDefaultExceiptionMessage(Exception defaultException){
+		Map<String, Object> errorMap = new HashMap<String, Object>();
+		
+		errorMap.put("errorCode", DefaultExceiptionErrorCode);
+		errorMap.put("message", "알수없는 이유의 오류가 발생했습니다.");
+		
+		return errorMap;
+	}
+
+	
 }
