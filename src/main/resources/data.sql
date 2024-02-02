ff701999d08241f0664b39ca0c91508588194a17
KAispread <pain0121@naver.com>
2024-02-02 18:25:37 +0900

    chore: add test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 DELETE FROM performance;
 DELETE FROM performance_seat_info;
+DELETE FROM reservation;
 
 INSERT INTO `performance` (name, price, round, type, start_date, is_reserve)
 VALUES ('ant', 100000, 1, 0, '2024-01-20 19:30:00', 'disable');
@@ -12,4 +13,5 @@ INSERT INTO performance_seat_info VALUES
 ,(DEFAULT, (SELECT id FROM performance limit 1), 1, 1, 'A', 5, 'disable', DEFAULT, DEFAULT);
 
 INSERT INTO `reservation` (performance_id, name, phone_number, round, gate, line, seat, created_at, updated_at) VALUES
-((SELECT id FROM performance limit 1), 'KAI', '01012345678', 1, 1, 'A', 4, DEFAULT, DEFAULT);
+((SELECT id FROM performance limit 1), 'KAI', '01012345678', 1, 1, 'A', 4, DEFAULT, DEFAULT)
+,((SELECT id FROM performance limit 1), 'KUN', '01099885678', 1, 1, 'A', 5, DEFAULT, DEFAULT);
