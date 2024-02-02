1d59d3e43c003ae25e8916ab2c8972ba20394b7d
KAispread <pain0121@naver.com>
2024-02-02 18:34:06 +0900

    chore: add JPAQueryFactory bean

--- src/main/java/com/wanted/preonboarding/ticket/config/JpaConfig.java
+++ src/main/java/com/wanted/preonboarding/ticket/config/JpaConfig.java
@@ -1,10 +1,16 @@
 package com.wanted.preonboarding.ticket.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {
-
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
 }
