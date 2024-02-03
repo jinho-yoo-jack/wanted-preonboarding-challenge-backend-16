db3ff34298ab95289baadfd00a5dd8ec27b7a2b2
jsoyun <syusmm233@gmail.com>
2024-02-03 16:35:22 +0900

    Fix: Set performanceId to unique

--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/Reservation.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/Reservation.java
@@ -6,11 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
-import java.sql.Date;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Entity
@@ -23,7 +19,7 @@ public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(columnDefinition = "BINARY(16)", nullable = false, name = "performance_id")
+    @Column(columnDefinition = "BINARY(16)", nullable = false, name = "performance_id", unique = true)
     private UUID performanceId;
     @Column(nullable = false)
     private String name;
@@ -34,16 +30,17 @@ public class Reservation {
     private int gate;
     private char line;
     private int seat;
+
     public static Reservation of(ReserveInfo info) {
         return Reservation.builder()
-            .performanceId(info.getPerformanceId())
-            .name(info.getReservationName())
-            .phoneNumber(info.getReservationPhoneNumber())
-            .round(info.getRound())
-            .gate(1)
-            .line(info.getLine())
-            .seat(info.getSeat())
-            .build();
+                .performanceId(info.getPerformanceId())
+                .name(info.getReservationName())
+                .phoneNumber(info.getReservationPhoneNumber())
+                .round(info.getRound())
+                .gate(1)
+                .line(info.getLine())
+                .seat(info.getSeat())
+                .build();
     }
 
 }
