18fa0c2711d00aa0d01321c095b91e41742f7482
jsoyun <syusmm233@gmail.com>
2024-02-03 16:38:27 +0900

    Fix: findByNameAndPhoneNumber return List<Reservation>

--- src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/ReservationRepository.java
+++ src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/ReservationRepository.java
@@ -3,12 +3,14 @@ package com.wanted.preonboarding.ticket.infrastructure.repository;
 import com.wanted.preonboarding.ticket.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
-    Optional<Reservation> findByNameAndPhoneNumber(String name, String phoneNumber);
+    Optional<List<Reservation>> findByNameAndPhoneNumber(String name, String phoneNumber);
+
     void deleteByPerformanceIdAndNameAndPhoneNumberAndRound(
-            UUID performanceId,String name,String phoneNumber, int round
+            UUID performanceId, String name, String phoneNumber, int round
     );
 }
