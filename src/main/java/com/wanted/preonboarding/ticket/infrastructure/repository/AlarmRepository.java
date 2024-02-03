c31c97523105270f827c88a3a52c853e7146f7e4
brightGarden02 <rreone2017@gmail.com>
2024-02-03 22:58:53 +0900

    refactor: alarmRepository id 데이터타입 수정

--- src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/AlarmRepository.java
+++ src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/AlarmRepository.java
@@ -3,5 +3,5 @@ package com.wanted.preonboarding.ticket.infrastructure.repository;
 import com.wanted.preonboarding.ticket.domain.entity.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+public interface AlarmRepository extends JpaRepository<Alarm, Integer> {
 }
