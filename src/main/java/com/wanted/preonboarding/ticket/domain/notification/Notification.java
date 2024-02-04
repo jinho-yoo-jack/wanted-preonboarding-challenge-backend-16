06c3d63cc01a8b75996c3322d2ee87a1f39c9def
KAispread <pain0121@naver.com>
2024-02-04 20:33:30 +0900

    refactor: change id type to Integer

--- src/main/java/com/wanted/preonboarding/ticket/domain/notification/Notification.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/notification/Notification.java
@@ -20,7 +20,7 @@ public class Notification extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false)
     private String targetId;
