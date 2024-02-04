93c3efedd09e448e2ea9bf403c06f58a2497a769
KAispread <pain0121@naver.com>
2024-02-04 17:29:39 +0900

    feat: add query to find Notification Entity

--- src/main/java/com/wanted/preonboarding/ticket/domain/notification/NotificationRepository.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/notification/NotificationRepository.java
@@ -1,7 +1,9 @@
 package com.wanted.preonboarding.ticket.domain.notification;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
 
+    List<Notification> findAllByTargetId(String targetId);
 }
