560ea463b2550a1f37cdb8792a6b265a4f2fe7b3
KAispread <pain0121@naver.com>
2024-02-04 18:40:38 +0900

    refactor: change id type to Long

--- src/main/java/com/wanted/preonboarding/ticket/domain/notification/Notification.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/notification/Notification.java
@@ -20,7 +20,7 @@ public class Notification extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false)
     private String targetId;
