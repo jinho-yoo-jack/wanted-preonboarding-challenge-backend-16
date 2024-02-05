59e9fbdd64e39a9009c46718929e89fedfb55065
prlhspt <prlhspt@gmail.com>
2024-02-05 23:36:56 +0900

    JsonCreator 사용 안하므로 제거

--- src/main/java/com/wanted/preonboarding/ticket/domain/PerformanceIsReserve.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/PerformanceIsReserve.java
@@ -1,9 +1,5 @@
 package com.wanted.preonboarding.ticket.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
-import java.util.stream.Stream;
-
 public enum PerformanceIsReserve {
     ENABLE("활성화"), DISABLE("비활성화");
 
@@ -11,13 +7,5 @@ public enum PerformanceIsReserve {
 
     PerformanceIsReserve(String description) {
         this.description = description;
-    }
-
-    @JsonCreator
-    public static PerformanceIsReserve parsing(String inputValue) {
-        return Stream.of(values())
-                .filter(performanceIsReserve -> performanceIsReserve.toString().equals(inputValue.toUpperCase()))
-                .findFirst()
-                .orElse(null);
     }
 }
