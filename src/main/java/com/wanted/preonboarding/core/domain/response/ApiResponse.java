d4a2a8e714d44a673d4ef7adb64fe5c174b3036d
Bombo <bombo96@chungwoon.ac.kr>
2024-02-06 23:55:08 +0900

    refactor: @JsonFormat을 사용하지 않고 LocalDateTime의 직렬화를 커스터마이징 하도록 변경
    - LocalDateTimeSerializeCustomer#jackson2ObjectMapperBuilderCustomizer() : LocalDateTime의 직렬화를 "yyyy-MM-dd'T'HH:mm:ss 로 변경

--- src/main/java/com/wanted/preonboarding/core/domain/response/ApiResponse.java
+++ src/main/java/com/wanted/preonboarding/core/domain/response/ApiResponse.java
@@ -20,7 +20,6 @@ public record ApiResponse<T>(
 
         T data,
 
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime serverTime
 ) {
 
