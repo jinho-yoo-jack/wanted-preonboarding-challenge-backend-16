27d920c30ceaf69f43c53eb7d6c03f907d955252
antidote <>
2024-02-07 15:17:36 +0900

    feat/performanceApp test코드 작성

--- src/test/java/com/wanted/preonboarding/ticket/application/PerformanceAppTest.java
+++ src/test/java/com/wanted/preonboarding/ticket/application/PerformanceAppTest.java
@@ -1,12 +1,16 @@
 package com.wanted.preonboarding.ticket.application;
 
 import com.wanted.preonboarding.ticket.domain.dto.CreatedPerformanceRequestDto;
+import com.wanted.preonboarding.ticket.domain.dto.PerformanceInfo;
 import com.wanted.preonboarding.ticket.domain.entity.Performance;
 import com.wanted.preonboarding.ticket.domain.entity.PerformanceSeatInfo;
 import com.wanted.preonboarding.ticket.infrastructure.repository.PerformanceRepository;
 import com.wanted.preonboarding.ticket.infrastructure.repository.PerformanceSeatRepository;
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityNotFoundException;
+import org.aspectj.lang.annotation.RequiredTypes;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -18,33 +22,29 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+
+
 class PerformanceAppTest {
-
-
     @InjectMocks
     private PerformanceApp performanceApp;
-
     @Mock
     private PerformanceSeatRepository performanceSeatRepository;
-
     @Mock
     private PerformanceRepository performanceRepository;
 
-    @Test
-    @DisplayName("createdPerformance")
-    void create() throws Exception {
-        // Initialize mocks
+    CreatedPerformanceRequestDto requestDto;
+    Performance performance ;
+    @BeforeEach
+    void setUp() {
         MockitoAnnotations.initMocks(this);
-
-        // Mock data
-        CreatedPerformanceRequestDto requestDto =
-                CreatedPerformanceRequestDto.builder()
-                .performanceName("testPerformanceName")
+        requestDto = CreatedPerformanceRequestDto.builder()
+                        .performanceName("testPerformanceName")
                         .price(10000)
                         .round(1)
                         .type(1)
@@ -52,24 +52,41 @@ class PerformanceAppTest {
                         .lineRange("A").seatRange("1~3")
                         .build();
 
+        performance = Performance.of(requestDto);
+
+    }
+
+    @Test
+    @DisplayName("createdPerformance")
+    void create() throws Exception {
         List<String> actualSeatInfo = requestDto.generateCombinations();
         List<PerformanceSeatInfo> actualSeatList = new ArrayList<>();
         for (String seatInfo : actualSeatInfo) {
             actualSeatList.add(PerformanceSeatInfo.convertSeatInfo(seatInfo));
         }
-        //Test Data Create
         List<PerformanceSeatInfo> expectedSeatList = new ArrayList<>();
         expectedSeatList.add(PerformanceSeatInfo.convertSeatInfo("A1"));
         expectedSeatList.add(PerformanceSeatInfo.convertSeatInfo("A2"));
         expectedSeatList.add(PerformanceSeatInfo.convertSeatInfo("A3"));
 
-
         performanceApp.create(requestDto);
 
         assertEquals(expectedSeatList.size(), actualSeatList.size());
 
-        verify(performanceSeatRepository, times(1)).saveAll(anyList());
-        verify(performanceRepository, times(1)).save(any(Performance.class));
+    }
+
+    @Test
+    @DisplayName("getDetail")
+    void getDetail() throws Exception {
+        //given
+        when(performanceRepository.findByName("testPerformanceName")).thenReturn(Optional.of(performance));
+
+        //when
+        PerformanceInfo performanceInfo = performanceApp.getDetail("testPerformanceName");
+
+        //then
+        assertEquals(performanceInfo.getPerformanceName(),"testPerformanceName");
+
     }
 
 }
